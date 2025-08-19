719ea7375df6e8ed50e2305e411dfc721496012f
09mccroner <robster.mccrone@gmail.com>
2025-08-19 12:44:02 +0100

    fix pivots check again

--- src/pivot/pivot_points.rs
+++ src/pivot/pivot_points.rs
@@ -21,6 +21,7 @@ pub struct Pivot {
 }
 
 #[derive(Debug, Clone)]
+#[derive(PartialEq)]
 pub enum PivotType {
     High,
     Low,
@@ -47,41 +48,49 @@ impl Next<&DataItem> for PivotPoints {
     fn next(&mut self, input: &DataItem) -> Self::Output {
         self.bars.pop_front();
         self.bars.push_back(input.clone());
-        
-        if item_is_pivot_high(self.lookback_period, self.bars.clone()) {
+
+        if let Some(ph) = find_pivot_high(self.lookback_period, self.bars.clone()) {
             self.pivots.pop_front();
-            self.pivots.push_back(Pivot{price: Decimal::from_f64(input.high()).unwrap(), pivot_type: PivotType::High});
-        } else if item_is_pivot_high(self.lookback_period, self.bars.clone()) {
+            self.pivots.push_back(Pivot{price: Decimal::from_f64(ph).unwrap(), pivot_type: PivotType::High});
+        }
+
+        if let Some(pl) = find_pivot_low(self.lookback_period, self.bars.clone()) {
             self.pivots.pop_front();
-            self.pivots.push_back(Pivot{price: Decimal::from_f64(input.low()).unwrap(), pivot_type: PivotType::Low});
+            self.pivots.push_back(Pivot{price: Decimal::from_f64(pl).unwrap(), pivot_type: PivotType::Low});
         }
         
         self.pivots.clone()
     }
 }
 
-fn item_is_pivot_high(period: usize, b: VecDeque<DataItem>) -> bool {
-    for i in 0..period {
-        if !(b[i].high() < b[i + 1].high()) {
-            return false
-        };
-        if !(b[period * 2 - i].high() > b[period * 2 - i - 1].high()) {
-            return false;
+fn find_pivot_high(period: usize, b: VecDeque<DataItem>) -> Option<f64> {
+    for i in 0..2 * period {
+        if i >= period {
+            if b[i].high() <= b[i+1].high() {
+                return None;
+            };
+        } else {
+            if b[i].high() >= b[i+1].high() {
+                return None;
+            };
         }
     }
-    true
+    Some(b[period].high())
 }
 
-fn item_is_pivot_low(period: usize, b: VecDeque<DataItem>) -> bool {
-    for i in 0..period {
-        if !(b[i].low() > b[i + 1].low()) {
-            return false
-        };
-        if !(b[period * 2 - i].low() < b[period * 2 - i - 1].low()) {
-            return false;
-        };
+fn find_pivot_low(period: usize, b: VecDeque<DataItem>) -> Option<f64> {
+    for i in 0..2 * period {
+        if i >= period {
+            if b[i].low() >= b[i+1].low() {
+                return None;
+            };
+        } else {
+            if b[i].low() <= b[i+1].low() {
+                return None;
+            };
+        }
     }
-    true
+    Some(b[period].low())
 }
 
 impl Default for PivotPoints {
@@ -98,4 +107,64 @@ fn default_bar() -> Result<DataItem> {
         .low(0.0)
         .volume(0.0)
         .build()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ta::Next;
+    use crate::PivotType::{High, Low};
+
+    fn setup_di_highs(high: f64) -> DataItem {
+        DataItem::builder()
+            .open(high)
+            .close(high)
+            .high(high)
+            .low(high)
+            .volume(high)
+            .build()
+            .unwrap()
+    }
+
+    fn setup_di_lows(low: f64) -> DataItem {
+        DataItem::builder()
+            .open(low)
+            .close(low)
+            .high(low)
+            .low(low)
+            .volume(low)
+            .build()
+            .unwrap()
+    }
+
+    #[test]
+    fn test_pivot_points_high() {
+        let mut pp = PivotPoints::new(2, 3).unwrap();
+
+        pp.next(&setup_di_highs(0.1));
+        pp.next(&setup_di_highs(0.2));
+        pp.next(&setup_di_highs(0.3));
+        pp.next(&setup_di_highs(0.2));
+        let out = pp.next(&setup_di_highs(0.1));
+
+        println!("{:?}", out);
+        println!("{:?}", pp);
+
+        assert_eq!(out.back().unwrap().price, dec!(0.3));
+        assert_eq!(out.back().unwrap().pivot_type, High);
+    }
+
+    #[test]
+    fn test_pivot_points_low() {
+        let mut pp = PivotPoints::new(2, 3).unwrap();
+
+        pp.next(&setup_di_lows(0.3));
+        pp.next(&setup_di_lows(0.2));
+        pp.next(&setup_di_lows(0.1));
+        pp.next(&setup_di_lows(0.2));
+        let out = pp.next(&setup_di_lows(0.3));
+
+        assert_eq!(out.back().unwrap().price, dec!(0.1));
+        assert_eq!(out.back().unwrap().pivot_type, Low);
+    }
 }
